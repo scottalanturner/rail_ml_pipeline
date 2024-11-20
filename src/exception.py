bc904ced6c01fe462d0380daa96f613ea24b349b
Scott Alan Turner <turner655+github@gmail.com>
2024-11-20 08:53:17 -0600

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,22 @@
+import sys
+
+def error_message_detail(error, error_detail: sys):
+    """
+    Generate detailed error message including file name and line number
+    """
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occurred in python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
+    return error_message
+
+class CustomException(Exception):
+    """
+    Custom exception class for ML pipeline that provides detailed error information
+    """
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+    
+    def __str__(self):
+        return self.error_message
+
